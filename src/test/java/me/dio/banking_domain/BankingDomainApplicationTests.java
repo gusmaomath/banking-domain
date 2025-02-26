4471a277eed21e792b0526546fd96acc8505dacf
Matheus Gusmão <gusmaomatheusdev@gmail.com>
2025-02-26 18:35:43 -0300

    Iniciando Projeto

---
+++ src/test/java/me/dio/banking_domain/BankingDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.banking_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
